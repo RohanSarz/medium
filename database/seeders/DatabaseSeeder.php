a1caac609915627971f806e6afe3176d5aa2cca1
Rohan <rjrpic@gmail.com>
2025-10-20 00:20:46 +0600

    created post and category, added fake data seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Catergory;
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,10 +19,17 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        $categories = ['Technology', 'Health', 'Science', 'Sports', 'Politics', 'Entertainment'];
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::create([
+        //     'name' => 'Rohan',
+        //     'email' => 'rjrpic@gmail.com',
+        //     'password' => bcrypt('rjrpic@gmail.com'),
+        // ]);
+
+        foreach ($categories as $category) {
+            Category::create(['name' => $category]);
+        }
+        Post::factory(100)->create();
     }
 }
